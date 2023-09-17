16830a6ef42db4055a809758bd35c630474a1fca
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-09-17 06:25:19 +0530

    make add delete edit controller routes and modelds

--- models/product.js
+++ models/product.js
@@ -1,17 +1,29 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product{
-  constructor(title, price, description, imageUrl){
+  constructor(title, price, description, imageUrl, id){
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save(){
     const db = getDb();
-    return db.collection('products')
-      .insertOne(this)
+    let dbOP;
+    if(this._id){
+      //Update the product
+      dbOP = db
+        .collection('products')
+        .updateOne({_id: this._id},{$set: this});
+
+    }else{
+      dbOP =  db.collection('products')
+      .insertOne(this);
+    }
+    return dbOP
       .then(result =>{
         console.log(result);
       })
@@ -34,6 +46,35 @@ class Product{
       .catch(err =>{
         console.log(err);
       })
+  }
+
+  static findById(prodId){
+    const db = getDb();
+    return db
+    .collection('products')
+    .find({_id: new mongodb.ObjectId(prodId)})
+    .next()
+    .then(product =>{
+      console.log(product);
+      return product;
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+
+  static deleteById(prodId){
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({_id: new mongodb.ObjectId(prodId)})
+      .then((result)=>{
+        console.log('Deleted');
+      })
+      .catch(err =>{
+        console.log(err);
+      } 
+      )
   }
 }
 

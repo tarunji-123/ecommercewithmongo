c4cc456a28beec7db50a4efcfb3f66f66a62b1e5
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-09-17 09:33:55 +0530

    add user

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("650652cc42a69d780f9f959f")
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));

f05c907975b4813ead4970719c320423c954f9c1
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-09-17 06:55:40 +0530

    add user

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
-
+const User = require('./models/user')
 
 const app = express();
 
@@ -19,13 +19,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById("650652cc42a69d780f9f959f")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+
 });
 
 app.use('/admin', adminRoutes);
